403d6cfe276dd07bddd17859e140f865e04d0da8
Joel Berger <joel.a.berger@gmail.com>
2014-01-25 12:47:32 -0600

    avoid copying "is windows" check into child

--- lib/Mojo/IOLoop/ForkCall.pm
+++ lib/Mojo/IOLoop/ForkCall.pm
@@ -2,16 +2,17 @@ package Mojo::IOLoop::ForkCall;
 
 use Mojo::Base 'Mojo::EventEmitter';
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 $VERSION = eval $VERSION;
 
 use Mojo::IOLoop;
 use IO::Pipely 'pipely';
 use POSIX ();
+
 use Perl::OSType 'is_os_type';
+use constant IS_WINDOWS => is_os_type('Windows');
 
 use Exporter 'import';
-
 our @EXPORT_OK = qw/fork_call/;
 
 has 'ioloop'       => sub { Mojo::IOLoop->singleton };
@@ -45,7 +46,7 @@ sub run {
 
     # attempt to generalize exiting from child cleanly on all platforms
     # adapted from POE::Wheel::Run mosty
-    eval { POSIX::_exit(0) } unless is_os_type('Windows');
+    eval { POSIX::_exit(0) } unless IS_WINDOWS;
     eval { CORE::kill KILL => $$ };
     exit 0;
 
